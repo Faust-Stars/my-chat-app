bdb14135c5cf46786f39aca21e29f323169fbdfb
Bogdan Way <bogdan.way.00.@gmail.com>
2024-08-19 13:22:36 +0500

    Initialcommit

---
+++ README.md
@@ -0,0 +1,93 @@
+# My-chat-app
+
+"My-chat-app" is a full-stack chatting application. It uses Socket.io for real-time communication and stores user details in an encrypted format in a MongoDB database.
+
+## Tech Stack
+
+**Client:** React JS
+
+**Server:** Node JS, Express JS
+
+**Database:** MongoDB
+
+## Obtaining API Keys and Tokens
+
+You need to register with cloudinary and take the necessary data from there for the 105th line of the Signup file.js https://api.cloudinary.com/v1_1/{Insert_your_username}/image/upload. In addition, you need to set your own values in .env PORT, MONGODB_URI, JWT_SECRET.
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/Faust-Stars/my-chat-app
+```
+
+Go to the project directory
+
+```bash
+  cd my-chat-app
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+```bash
+  cd frontend/
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+Start the Client
+
+```bash
+  //open now terminal
+  cd frontend
+  npm start
+```
+
+# Features
+
+### Authenticaton
+
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/login.PNG)
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/signup.PNG)
+
+### Real-Time Chatting with Typing indicators and Adaptive
+
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/real-time_and_adaptive.PNG)
+
+### One to One chat
+
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/mainscreen.PNG)
+
+### Search Users
+
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/search.PNG)
+
+### Create Group Chats
+
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/create_new_group.PNG)
+
+### Notifications
+
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/group_and_notification.PNG)
+
+### Add or Remove users from group
+
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/add_and_remove_users.PNG)
+
+### View Other user Profile
+
+![](https://github.com/Faust-Stars/my-chat-app/blob/master/screenshots/profile.PNG)
+
+## Made By
+
+- [@Faust-Stars](https://github.com/Faust-Stars)
---
+++ frontend/src/components/Authentication/Signup.js
@@ -0,0 +1,201 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+
+const Signup = () => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+  const toast = useToast();
+  const history = useHistory();
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [picture, setPicture] = useState();
+  const [pictureLoading, setPictureLoading] = useState(false);
+
+  const submitHandler = async () => {
+    setPictureLoading(true);
+    if (!name || !email || !password || !confirmPassword) {
+      toast({
+        title: "Please Fill all the Fields",
+        status: "warning",
+        duration: 5000,
+        position: "bottom",
+      });
+      setPictureLoading(false);
+      return;
+    }
+    if (password !== confirmPassword) {
+      toast({
+        title: "Passwords Do Not Match",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user",
+        { name, email, password, picture },
+        config
+      );
+      toast({
+        title: "Registration Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+
+      localStorage.setItem("userInfo", JSON.stringify(data));
+
+      setPictureLoading(false);
+      history.push("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPictureLoading(false);
+    }
+  };
+
+  const postDetails = (pictures) => {
+    setPictureLoading(true);
+    if (pictures === undefined) {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if (pictures.type === "image/jpeg" || pictures.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pictures);
+      data.append("upload_preset", "my-chat-app");
+      data.append("cloud_name", "dzoenweky");
+      fetch("https://api.cloudinary.com/v1_1/dzoenweky/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPicture(data.url.toString());
+          console.log(data.url.toString());
+          setPictureLoading(false);
+        })
+        .catch((err) => {
+          setPictureLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setPictureLoading(false);
+      return;
+    }
+  };
+
+  return (
+    <VStack spacing="5px">
+      <FormControl id="first-name" isRequired>
+        <FormLabel>Name</FormLabel>
+        <Input
+          placeholder="Enter Your Name"
+          onChange={(e) => setName(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input
+          placeholder="Enter Your Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password" isRequired>
+        <FormLabel>Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="confirmPassword" isRequired>
+        <FormLabel>Confirm Password</FormLabel>
+        <InputGroup size="md">
+          <Input
+            type={show ? "text" : "password"}
+            placeholder="Confirm password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+      <FormControl id="picture" isRequired>
+        <FormLabel>Upload your Picture</FormLabel>
+        <Input
+          type="file"
+          p={1.5}
+          accept="image/*"
+          placeholder="Confirm password"
+          onChange={(e) => postDetails(e.target.files[0])}
+        />
+      </FormControl>
+      <Button
+        colorScheme="blue"
+        width="100%"
+        style={{ marginTop: 15 }}
+        onClick={submitHandler}
+        isLoading={pictureLoading}
+      >
+        Sign Up
+      </Button>
+    </VStack>
+  );
+};
+
+export default Signup;
